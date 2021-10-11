f50e11a35198af2d4c87e0f9d73765b367ca7861
Mark Voltaire <markjvoltaire@gmail.com>
2021-10-11 18:35:24 -0400

    photo and button added

---
+++ render-sneakers.js
@@ -0,0 +1,28 @@
+
+
+export function renderSneakers(shoe){
+    const sneakerDisplay = document.createElement('div');
+    sneakerDisplay.classList.add('sneaker-Display');
+
+    const sneakerHeader = document.createElement('h2');
+    sneakerHeader.textContent = shoe.brand;
+
+    const img = document.createElement('img');
+    img.src = shoe.img;
+
+    const addButton = document.createElement('button');
+    addButton.textContent = 'add to cart';
+
+    sneakerDisplay.append(sneakerHeader, img, addButton );
+    return sneakerDisplay
+
+
+
+   
+
+
+
+
+
+
+}
