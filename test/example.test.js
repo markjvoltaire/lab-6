9f25934fc3976e2082f371d1933fe071bc5423bc
Mark Voltaire <markjvoltaire@gmail.com>
2021-10-15 18:18:21 -0400

    eslint fix v2

--- test/example.test.js
+++ test/example.test.js
@@ -1,7 +1,7 @@
 // IMPORT MODULES under test here:
 import { shoes } from '../Data/shoes.js';
-import { renderSneakers } from './render-sneakers.js';
-import { findById } from  './utils'
+
+import { findById } from './utils';
 
 // import { example } from '../example.js';
 
@@ -9,23 +9,23 @@ const test = QUnit.test;
 
 test('time to test a function', (expect) => {
    
-   const expected = {
-    id: '2',
-    brand: 'Jordans',
-    type: 'low top',
-    img: 'assets/sneakers pics/cement-3.png'
+    const expected = {
+        id: '2',
+        brand: 'Jordans',
+        type: 'low top',
+        img: 'assets/sneakers pics/cement-3.png'
        
-   }
+    };
    
    
-   const actual = findById ('4',shoes )
+    const actual = findById ('4', shoes);
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    
 
     //Expect
     // Make assertions about what is expected versus the actual result
